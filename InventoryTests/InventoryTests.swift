e98cfdc1beea6c8332154e16d71025a5941a84f4
Somebud0180 <ethanlagera@icloud.com>
2025-07-03 08:25:20 +0800

    Initial Commit

---
+++ InventoryTests/InventoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryTests.swift
+//  InventoryTests
+//
+//  Created by Ethan John Lagera on 7/3/25.
+//
+
+import Testing
+@testable import Inventory
+
+struct InventoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
